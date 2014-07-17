9e4c8d38d72768829130c7f09f673986c1e6b0c7
unknown <KR6@TR33-BLR-DSI.dsone.3ds.com>
2014-07-18 03:43:44 +0530

    First commit

--- src/com/delta/finalproject/Transport.java
+++ src/com/delta/finalproject/Transport.java
@@ -1,12 +1,14 @@
 package com.delta.finalproject;
 
-import android.app.Activity;
+import android.app.TabActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
 
-public class Transport extends Activity{
+public class Transport extends TabActivity{
 	Button bBack;
 
 	@Override
@@ -25,6 +27,33 @@ public class Transport extends Activity{
 			}
 		});
 
-	}
-
+		         
+		        TabHost tabHost = getTabHost();
+		         
+		        // Tab for Photos
+		        TabSpec photospec = tabHost.newTabSpec("Auto");
+		        // setting Title and Icon for the Tab
+		        photospec.setIndicator("Auto", getResources().getDrawable(R.drawable.auto));
+		        Intent photosIntent = new Intent(this, Auto.class);
+		        photospec.setContent(photosIntent);
+		         
+		        // Tab for Songs
+		        TabSpec songspec = tabHost.newTabSpec("Taxi");        
+		        songspec.setIndicator("Taxi", getResources().getDrawable(R.drawable.taxi));
+		        Intent songsIntent = new Intent(this, Taxi.class);
+		        songspec.setContent(songsIntent);
+		         
+		        // Tab for Videos
+		        TabSpec videospec = tabHost.newTabSpec("Bus");
+		        videospec.setIndicator("Bus", getResources().getDrawable(R.drawable.bus));
+		        Intent videosIntent = new Intent(this, Bus.class);
+		        videospec.setContent(videosIntent);
+		         
+		        // Adding all TabSpec to TabHost
+		        tabHost.addTab(photospec); // Adding photos tab
+		        tabHost.addTab(songspec); // Adding songs tab
+		        tabHost.addTab(videospec); // Adding videos tab
+		    }
 }
+
+	
